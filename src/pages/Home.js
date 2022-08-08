a687fa8a425ffa162bbf98fa7c0db38c8f613338
ronsha001 <ronsh0111@gmail.com>
2022-08-08 21:03:07 +0300

    Finish Contact section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import About from '../components/About/About';
+import Contact from '../components/Contact/Contact';
 import Header from '../components/Header/Header';
 import Portfolio from '../components/Portfolio/Portfolio'
 
@@ -10,6 +11,7 @@ const Home = () => {
       <Header />
       <Portfolio sectionRef={myRefs[0]}/>
       <About />
+      <Contact />
     </>
   )
 }
