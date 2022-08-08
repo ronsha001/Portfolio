348f313ab5b8c524510273e9eff0a103258c5924
ronsha001 <ronsh0111@gmail.com>
2022-08-08 21:07:33 +0300

    Finish Footer section

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="text">
+          <small>Copyright © Ron Sharabi 2021 Israel</small>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react'
 import About from '../components/About/About';
 import Contact from '../components/Contact/Contact';
+import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import Portfolio from '../components/Portfolio/Portfolio'
 
@@ -12,6 +13,7 @@ const Home = () => {
       <Portfolio sectionRef={myRefs[0]}/>
       <About />
       <Contact />
+      <Footer />
     </>
   )
 }
